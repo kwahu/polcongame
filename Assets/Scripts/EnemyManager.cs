e72ae24ffb97ecdcd938e80bf77594ad52919aee
Arkadiusz Kwaśny <arkadiusz.kwasny@gmail.com>
2014-07-31 19:42:13 +0200

    new push player script

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyManager : MonoBehaviour {
+	float standUpTimer = 0.0f;
+	bool contact=false;
+	public float standUpTime = 3.0f;
+	public AudioClip downSound;
+	public AudioClip upSound;
+	// Use this for initialization
+	void Start () {
+		standUpTimer = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (contact) {
+			standUpTimer += Time.deltaTime;
+			if (standUpTimer > standUpTime) {
+				Capsule (upSound, false);
+				standUpTimer = 0.0f;
+			}
+		}
+	
+	}
+	void EnemyCheck(){
+		if(!contact){
+			Capsule(downSound, true);
+		}
+	}
+	void Capsule(AudioClip aClip, bool standCheck){
+		audio.PlayOneShot (aClip);
+		contact = standCheck;
+
+	}
+}
---
+++ Assets/Scripts/PlayerCollisions.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollisions : MonoBehaviour
+{
+
+		public float forceFactor = 10000;
+	
+		// Update is called once per frame
+		void Update ()
+		{
+
+				if (Input.GetButtonDown ("Fire1"))
+						Hit ();
+		}
+
+		void Hit ()
+		{
+				RaycastHit hit;
+
+				Physics.Raycast (transform.position, transform.forward, out hit, 10);
+				Debug.DrawLine (transform.position, hit.point, Color.green, 2, false);
+
+				if (hit.collider != null) {
+						Debug.Log ("hit:" + hit.collider.name);
+						GameObject obj = hit.collider.gameObject;
+				
+						if (obj.tag == "enemy") {
+								obj.rigidbody.AddForce (forceFactor * transform.forward);
+						}
+				}
+		}
+}
