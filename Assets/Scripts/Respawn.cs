8371976d5dbafd85afc753d078db3aba2a93e61e
Dawid Pośliński <dposlinski@gmail.com>
2014-08-02 23:08:30 +0200

    Coin scripts base

---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Respawn : MonoBehaviour {
+
+	public GameObject respawnedObject;
+	public int renewalTime = 20; // seconds
+	public bool respawned;
+
+	void Start()
+	{
+		RespawnObject();
+	}
+
+	void OnCollisionEnter(Collision collision) 
+	{
+		if ( respawned )
+		{
+			var coinCollector = collision.gameObject.GetComponent<CoinPlayer>();
+
+			if ( coinCollector != null )
+			{
+				coinCollector.GiveCoins(new List<Coin> { new Coin() });
+			}
+
+			respawned = false;
+		}
+	}
+	
+	void Update () 
+	{
+		if ( !respawned )
+		{
+			RespawnWithDelay();
+		}
+	}
+
+	IEnumerator StartWait()
+	{
+		yield return StartCoroutine(Wait());
+	}
+
+	IEnumerator Wait()
+	{
+		yield return new WaitForSeconds(renewalTime);      
+	}
+
+	void RespawnWithDelay()
+	{
+		respawned = true;
+
+		StartWait();
+
+		RespawnObject();
+	}
+
+	void RespawnObject()
+	{
+		respawned = true;
+	}
+}
