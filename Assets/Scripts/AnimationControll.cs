cdf7f8aec80434d1f15868fe7b942850efbfaa65
Robert Norenberg <robert@roberts-mbp.home>
2014-08-30 19:13:05 +0200

    make book hero animated

---
+++ Assets/Scripts/AnimationControll.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationControll : MonoBehaviour {
+
+	private Animator animator;
+	
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+
+	void FixedUpdate () {
+		transform.localPosition = Vector3.zero;
+
+		float sqrmag = transform.parent.rigidbody.velocity.sqrMagnitude;
+
+		if(sqrmag < 0.01){
+			sqrmag = 0.0f;
+		}
+
+		animator.SetFloat("Speed",  sqrmag);
+	}
+}
