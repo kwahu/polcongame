6a703e26a70fe58fd4d6962815f234f8f1d68cc4
Arkadiusz Kwaśny <arkadiusz.kwasny@gmail.com>
2014-08-31 20:27:01 +0200

    chest type matched with hero type

--- Assets/Scripts/coins/Chest.cs
+++ Assets/Scripts/coins/Chest.cs
@@ -6,13 +6,13 @@ public class Chest : Photon.MonoBehaviour {
 
 	void OnTriggerEnter(Collider other)
 	{
+		if (other.GetComponent<PlayerType> ().heroType == this.GetComponent<PlayerType> ().heroType) {
+						// TOCO check on trigger enter with right chest that belongs to player
+						var coinCollector = other.gameObject.GetComponent<PlayerNetwork> ();
 
-		// TOCO check on trigger enter with right chest that belongs to player
-		var coinCollector = other.gameObject.GetComponent<PlayerNetwork>();
-
-		if ( coinCollector != null )
-		{	
-			coinCollector.MoveCoinsToChest();
-		}
+						if (coinCollector != null) {	
+								coinCollector.MoveCoinsToChest ();
+						}
+				}
 	}
 }
